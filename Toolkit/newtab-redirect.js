0ec971433c7ff77f43410b56aef6a4561fa7f238
HauseMaster <113833707+HauseMasterZ@users.noreply.github.com>
2026-08-06 23:03:18 +0530

    feat: add dark mode

--- Toolkit/newtab-redirect.js
+++ Toolkit/newtab-redirect.js
@@ -1,5 +1,6 @@
 chrome.storage.local.get({ featureNewTabPage: true }, (res) => {
-  if (!res.featureNewTabPage) {
+  // If the feature is disabled, or we are in an incognito window, revert to the native new tab
+  if (!res.featureNewTabPage || chrome.extension.inIncognitoContext) {
     chrome.tabs.update({ url: 'chrome://new-tab-page/' });
   } else {
     document.documentElement.style.display = '';
